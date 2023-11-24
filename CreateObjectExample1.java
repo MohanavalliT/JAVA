16ae909b6a31f90c7becf68680fb3b6d4fe05a8b
MOHANAVALLI T <118752312+MohanavalliT@users.noreply.github.com>
2023-11-24 22:00:02 +0530

    Add files via upload

---
+++ CreateObjectExample1.java
@@ -0,0 +1,12 @@
+public class CreateObjectExample1   
+{    
+void show()    
+{    
+System.out.println("Welcome to javaTpoint");    
+}    
+public static void main(String[] args)   
+{     
+CreateObjectExample1 obj = new CreateObjectExample1();    
+obj.show();    
+}    
+}   
